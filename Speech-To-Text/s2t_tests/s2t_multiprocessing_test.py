ffabc6f2d0c5898312121ae05ef0c6d95a2a2853
Robin <robin10125@gmail.com>
2023-12-11 14:25:57 -0500

    Cleaning code

--- Speech-To-Text/s2t_tests/s2t_multiprocessing_test.py
+++ Speech-To-Text/s2t_tests/s2t_multiprocessing_test.py
@@ -12,7 +12,8 @@ from faster_whisper import WhisperModel
 # TODO: Switch from threading to multiprocessing module to utulize CPU more effectively for larger models
 #       Set up algorithm to chunk audio and text based off of periods of silence
 #       Feed text chunks to LLM
-
+#       Deal with multiprocessing termination
+#       Set up queue structure
 
 ###--- Audio recording parameters ---###
 FORMAT = pyaudio.paInt16
@@ -25,9 +26,60 @@ device_id = 6
 ###--- End Audio recording parameters ---###
 
 ###############################################################################################
+######------ Test code for multiprocessing ------######
+
+def model_server(input_queue, output_queue):
+    
+    model = initialize_model()
+    while True:
+        #Receive audio data from the recording program
+        #TODO check sync of input_queue.get
+        audio_data = input_queue.get()
+
+        # Transcribe the audio data into segments of text
+        segments = model.transcribe(audio_data, beam_size=5, vad_filter=True, word_timestamps=True) 
+        
+        # Send the result to the display program
+        output_queue.put(process_transcript(segments))
+
+def process_transcript(segments):
+
+    trans_chunk = False
+    output_string= ""
+    #print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+    for segment in segments:
+        for word in segment.words:
+            
+            #check words that exist across chunks.  These words would be duplicated in the output
+            if word.start <= RECORD_SECONDS + OVERLAP_SECONDS and word.end >= RECORD_SECONDS + OVERLAP_SECONDS and not trans_chunk:
+                trans_chunk = True
+                #output_string += "(trans chunk word) [%.2fs -> %.2fs] %s" % (word.start, word.end, word.word)
+                output_string += word.word
+                
+            if (word.end <= RECORD_SECONDS+OVERLAP_SECONDS and word.start >= OVERLAP_SECONDS):
+                if not trans_chunk:
+                    #output_string += "[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word)
+                    output_string += word.word
+                trans_chunk = False
+
+    return output_string
+
+def initialize_model(size="tiny.en"):
+
+    model_size = size
+    # or run on GPU with INT8
+    # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+    # or run on CPU with INT8
+    model = WhisperModel(model_size, device="cpu", compute_type="int8")
+    return model
+
+######------ End Test code for multiprocessing ------######
+###############################################################################################
 
 ###--- Functions ---###
 
+#transcribe_audio_chunk handles the transcription of the audio chunk
 def transcribe_audio_chunk(filename, model):
 
     trans_chunk = False
@@ -48,7 +100,7 @@ def transcribe_audio_chunk(filename, model):
                     print(word.word, end="", flush=True)
                 trans_chunk = False
 
-# process_audio_chunks handles the audio processing, taking the frames and reference to the data model as inputs
+# process_audio_chunks handles the audio processing turning frames into a file like object
 def process_audio_chunk(frames, model, sample_width):
     
     #convert frames into wav file like BytesIO object
@@ -70,10 +122,6 @@ def start_recording_overlap(stream, model, sample_width):
     current_frames = []
     overlap_frames = []
 
-    next_chunk_start_time = int(time.time())
-
-    repository_name = f"session_file_chunks{next_chunk_start_time}"
-    os.makedirs(repository_name, exist_ok=True)
     try:
         while True:
             data = stream.read(CHUNK, exception_on_overflow=False)
@@ -87,34 +135,21 @@ def start_recording_overlap(stream, model, sample_width):
                 # Prepare the chunk for processing
                 processing_frames = current_frames
 
+                # Process the chunk
+                wav_stream = process_audio_chunk(processing_frames, model, sample_width)
+
                 # Update overlap for the next chunk
                 overlap_frames = current_frames[-int(2*OVERLAP_SECONDS * RATE / CHUNK):]
                 current_frames = overlap_frames.copy()
 
-                # Update the start time for the next chunk
-                next_chunk_start_time = int(time.time())
-
-                # Process the chunk
-                wav_stream = process_audio_chunk(processing_frames, model, sample_width)
-                #Transcribe the chunk in a separate thread
+                '''#Transcribe the chunk in a separate thread
                 processing_thread = threading.Thread(target=transcribe_audio_chunk, args=(wav_stream, model))
-                processing_thread.start()  
+                processing_thread.start() ''' 
     
     except KeyboardInterrupt:
         print("\nRecording stopped by user.")
 
-#main function
 def main():
-    
-    ###--------- Model Setup ---------###
-    model_size = "tiny.en"
-    #model_size = "base.en"
-
-    # or run on GPU with INT8
-    # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-    # or run on CPU with INT8
-    model = WhisperModel(model_size, device="cpu", compute_type="int8")
-    ###--------- End Model Setup ---------###
 
     ###---------Setup Audio Stream---------###
     pa = pyaudio.PyAudio()
@@ -125,18 +160,21 @@ def main():
                     input=True,
                     input_device_index=device_id,
                     frames_per_buffer=CHUNK)
-
-    input("Program started. Press any key to start recording.  Press Ctrl+C to stop.")
-    start_recording_overlap(stream, model, sample_width)
     ###---------End Setup Audio Stream---------###
 
+    input_queue = multiprocessing.Queue()
+    output_queue = multiprocessing.Queue()
+
+    input("Program started. Press any key to start recording.  Press Ctrl+C to stop.")
+    start_recording_overlap(stream, sample_width)
+
+    
     #Cleanup 
     stream.stop_stream()
     stream.close()
     pa.terminate()
 
 ###--- End Functions ---### 
-#####################################################################################################
 
 if __name__ == "__main__":
     main()
